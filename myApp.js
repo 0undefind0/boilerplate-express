d4a31515ff5877c9ab7ff775b0814e2ba03938b6
Martin <38350673+mstephjuan@users.noreply.github.com>
2024-11-04 14:27:59 +0000

    completed use-the-env-file

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 console.log("Hello World");
 
@@ -13,7 +14,11 @@ app.get("/", (req, res) => {
 })
 
 app.get("/json", (req, res) => {
-    res.json({"message": "Hello json"});
+    let message = "Hello json";
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+        message = message.toUpperCase();
+    }
+    res.json({"message": message});
 })
 
 
